fc9244ffe105358b27567255cf9c169b3f3fbd7e
tkyksasuka <110615564+tkyksasuka@users.noreply.github.com>
2022-11-20 15:28:38 +0000

    admin view 一通り触った記入、次回publicから

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,9 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
+    @order_detail = Order_detail.find(params[:id])
+    @order_detail.update
+    redirect_to
   end
 
 end
