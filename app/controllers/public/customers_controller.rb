7007f6b0dcdc4b0d8d0d88abdfd848ce96149141
tkyksasuka <110615564+tkyksasuka@users.noreply.github.com>
2022-11-30 15:46:00 +0000

    客画面作成中 order_detail

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,15 +1,18 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
+    @custmer = Custmer.find(params[:id])
   end
-  
+
   def edit
+    @custmer = Custmer.find(params[:id])
+    @custmer.update(custmer_params)
   end
-  
+
   def unsubscribe
   end
-  
-  
+
+
 
 
 
--- app/controllers/public/order_details_controller.rb
+++ app/controllers/public/order_details_controller.rb
@@ -1,6 +1,9 @@
 class Public::OrderDetailsController < ApplicationController
 
   def index
+    @order_detail_new = Order_detail.new
+    @order_detail_new.save
+    @order_details = Order_detail.all
   end
 
   def edit
